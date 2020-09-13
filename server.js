aeab9c153509a786000ac45a676817226575a29e
vyankatesh9604 <vyankateshgadpallewar12@gmail.com>
2020-09-13 00:14:21 -0700

    Changed port

--- server.js
+++ server.js
@@ -25,8 +25,8 @@ app.use('/users',userRoutes)
 //         message:"Hello from server"
 //     })
 // })
-const port = 5000 
+const PORT =process.env.PORT ||  5000 
 
-app.listen(port,() => {
+app.listen(PORT,() => {
     console.log("Sucessfully started ")
 })
